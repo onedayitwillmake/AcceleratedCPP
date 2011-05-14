fc08cc393473872faedf6d1b34376908a222b99e
onedayitwillmake <mariogonzalez@gmail.com>
2011-05-14 16:11:10 -0400

    Added Chapter4

---
+++ Chapter4Neu/Debug/src/AccCppChapter4.d
@@ -0,0 +1,6 @@
+src/AccCppChapter4.d src/AccCppChapter4.o: ../src/AccCppChapter4.cpp \
+  ../src/grade.h ../src/Student_info.h
+
+../src/grade.h:
+
+../src/Student_info.h:
---
+++ Chapter4Neu/Debug/src/grade.d
@@ -0,0 +1,8 @@
+src/grade.d src/grade.o: ../src/grade.cpp ../src/grade.h \
+  ../src/Student_info.h ../src/median.h
+
+../src/grade.h:
+
+../src/Student_info.h:
+
+../src/median.h:
